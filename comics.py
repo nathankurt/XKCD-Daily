8b201461d40082c3e15d7e1e0605c015ecca9501
Nate Kurt <nate.kurt@gmail.com>
2020-02-24 09:37:25 -0500

    updating changes

--- comics.py
+++ comics.py
@@ -12,7 +12,7 @@ def send_text(current_num,title, secret_text,image_url):
             body = f"XKCD Comic {current_num}: {title} \nAlt Text: {secret_text}",
             media_url=[image_url],
             from_= os.environ['TWILIO_PHONE_NUMBER'],
-            to=os.environ['TO_PHONE_NUMBER']
+            to = os.environ['TO_PHONE_NUMBER']
         )
 
 
@@ -22,8 +22,6 @@ def send_text(current_num,title, secret_text,image_url):
         
     except:
         print("ERROR: MESSAGE FAILED TO SEND")
-
-
 
 # IF SUCCESSFUL, update number
 
@@ -45,31 +43,13 @@ last_sent_num = [line.strip() for line in open("num.txt")][0]
 if (int(current_num) != int(last_sent_num)):
     #XKCD has been updated, go ahead and send it
     send_text(current_num,title,secret_text,image_url)
-    with open("num.txt", "w") as f:
-        f.write(str(current_num))
-    assert int(data['num']) == int([line.strip() for line in open("num.txt")][0]), f"ERROR: num.txt didn't update how it was supposed to \
-        num.txt = {[line.strip() for line in open('num.txt')][0]} And\
-        data['num'] = {data['num']} "
-
+    # with open("num.txt", "w") as f:
+    #     f.write(str(current_num))
+    # assert int(data['num']) == int([line.strip() for line in open("num.txt")][0]), f"ERROR: num.txt didn't update how it was supposed to \
+    #     num.txt = {[line.strip() for line in open('num.txt')][0]} And\
+    #     data['num'] = {data['num']} "
 
 
 else:
     #No new comic yet, don't continue
     exit()
-
-
-
-
-
-## Send Text
-
-
-
-
-
-
-print(title)
-print(image_url)
-print(secret_text)
-print(current_num)
-print(last_sent_num)
